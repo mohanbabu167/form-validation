b0522e70194a3d925a632147ef9c2f92201784f0
mohanbabu167 <143276482+mohanbabu167@users.noreply.github.com>
2024-02-24 13:55:53 +0530

    Add files via upload

---
+++ js project form valedation/app.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#form")
+const form = document.querySelector("#form")
+const form = document.querySelector("#form")
+const form = document.querySelector("#form")
+const form = document.querySelector("#form")
+ 
